7340602ccdd4202835a7961b89839f0ab9e0973f
Kiyomiddinova-Farida <farida.qiyomiddinova.09@gmail.com>
2025-07-06 16:47:09 +0500

    DDD file structure full created

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { AuthorModule } from './author/author.module';
+import { GenreModule } from './genre/genre.module';
+import { BookModule } from './book/book.module';
+import { BasketModule } from './basket/basket.module';
+import { ReviewModule } from './review/review.module';
+import { OrderModule } from './order/order.module';
+import { OrderItemModule } from './order-item/order-item.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, AuthorModule, GenreModule, BookModule, BasketModule, ReviewModule, OrderModule, OrderItemModule, UserModule],
   providers: [AppService],
 })
 export class AppModule {}
