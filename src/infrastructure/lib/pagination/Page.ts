c293844a2df534a4fbee2a5db3f262cbb8413c9d
Kiyomiddinova-Farida <farida.qiyomiddinova.09@gmail.com>
2025-07-06 20:21:13 +0500

    Page.ts file writed

--- src/infrastructure/lib/pagination/Page.ts
+++ src/infrastructure/lib/pagination/Page.ts
@@ -5,13 +5,3 @@ export interface Page<T> {
   page_size: number;
 }
 
-type customQueryOptionsDetails = {
-  query: string;
-  parameters: any[];
-};
-
-export interface findAllCustomQueryOptions {
-  data: customQueryOptionsDetails;
-  count: customQueryOptionsDetails;
-  take: number | undefined;
-}
